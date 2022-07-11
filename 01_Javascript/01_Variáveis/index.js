3d805ae47911d7a91890a4ddcd85c96fd801dfc4
Igor Campos <igorrcfernandes@gmail.com>
2022-07-11 15:17:40 -0300

    Atualizando

---
+++ 01_Javascript/01_Variáveis/index.js
@@ -0,0 +1,36 @@
+let nome = "André";
+let idade = 28;
+let possuiFaculdade = true;
+
+console.log (nome, idade, possuiFaculdade, nome);
+
+let preco = 25;
+let totalComprado = 5;
+let totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+let sobreNome = "Igor",
+    cidade = "Vitória";
+
+console.log(sobreNome, cidade);
+
+let semDefinir;
+
+console.log(semDefinir); //Undefined
+
+console.log(nome_2);
+let nome_2 = "Lucas"; //Cannot access 'nome_2' before initialization
+
+let profissao = "Programer";
+console.log(profissao);
+
+var idade_2 = 28;
+idade_2 = 29;
+
+let preco_2 = 50;
+preco_2 = 25;
+
+const possuiCarro = true; 
+possuiCarro = false;
+//Retorna um erro.
---
+++ 01_Javascript/02_Tipos de Dados/index.js
@@ -0,0 +1,21 @@
+let idade = 27;
+let simbolo = Symbol();
+console.log(typeof simbolo);
+
+let nome = 'Igor';
+let sobrenome = 'Campos';
+let nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto);
+
+let ano = '2018';
+let mes = 8;
+console.log(ano + mes);
+
+let melhor = 'teste';
+let frase1 = "Esse é o \"melhor\" jogo.";
+console.log(frase1);
+
+let gols = 1000;
+let frase2 = `Romário fez ${gols * 2} gols.`;
+console.log(frase2);
