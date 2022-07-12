691d05ec1314ed527578dd46aae1424fc1a3e02c
Igor Campos <igorrcfernandes@gmail.com>
2022-07-12 16:55:15 -0300

    Atualizando

---
+++ 01_Javascript/03_Números e Operadores/index.js
@@ -0,0 +1,25 @@
+let idade = 28;
+let gols = 1000;
+let pi = 3.14;
+let exp = 2e10;
+
+let soma = 100 + 50;
+let subtracao = 100 - 50;
+let multiplicacao = 100 * 2;
+let divisao = 100 / 2;
+let expoente = 2 ** 4;
+let modulo = 14 % 5;
+
+let soma2 = '100' + 50;
+let subtracao2 = '100' - 50;
+let multiplicacao2 = '100' * '2';
+let divisao2 = 'Comprei 10' / 2;
+
+let numero = 80;
+let unidade = 'kg';
+let peso = numero + unidade; // '80kg'
+let pesoPorDois = peso / 2 // NaN 
+
+let x = 5;
+console.log(++x);
+console.log(x);
